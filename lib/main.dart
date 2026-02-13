f52caa5e26274d7fc44bdfcf4df2f7749544a0fe
OnoPUNPUN <punpunonodera754@gmail.com>
2026-02-13 17:18:16 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:clean_architecture_project/core/theme/theme.dart';
+import 'package:clean_architecture_project/features/auth/presentation/pages/sign_up_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blog App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkThemeMode,
+      home: const SignUpScreen(),
+    );
+  }
+}
